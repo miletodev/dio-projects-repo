c47c90121152ed383e79b736cc771feda9cba02d
Mileto Almeida <almeidamirtilomileto@gmail.com>
2024-10-18 20:07:02 -0300

    Add: Adiocionando Interfaces e Repository

---
+++ src/main/java/com/miletodev/api_bancario/domain/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.miletodev.api_bancario.domain.repository;
+
+import com.miletodev.api_bancario.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    boolean existsByAccountNumber(String number);
+
+    boolean existsByCardNumber(String number);
+}
